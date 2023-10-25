02ecf2a6ab58f34fe564fe6b2498ad4147d3caad
loewleinmarlon <148561112+loewleinmarlon@users.noreply.github.com>
2023-10-25 11:46:45 +0200

    Update SEO.js

--- components/SEO.js
+++ components/SEO.js
@@ -3,6 +3,8 @@ import Head from 'next/head';
 export default function SEO({ title, description }) {
   return (
     <Head>
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7342102379369245"
+     crossorigin="anonymous"></script>
       <title>{title}</title>
       <meta name="description" content={description} />
       <meta property="og:title" content={title} />
